0fc68e9272252da85cff9efa1f5562b5737ddc16
pejuzb <36472683+pejuzb@users.noreply.github.com>
2024-09-19 20:22:58 +0200

    Update streamlit_app.py
    add requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
-
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -25,6 +24,9 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections = 5
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
 if ingredients_list:
