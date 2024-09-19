b5fe23e46d42d903d4d8f0ff036fcbaaa68799c6
pejuzb <36472683+pejuzb@users.noreply.github.com>
2024-09-19 20:33:24 +0200

    Update streamlit_app.py
    * change

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,6 @@ ingredients_list = st.multiselect(
     , max_selections = 5
 )
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
 
 if ingredients_list:
    # st.write(ingredients_list)
@@ -35,7 +32,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' '  
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
   #  st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
